8e49b5678e19c435e092f153e0211d83cdc8a704
Vincent Meijer <meijer.vincent@gmail.com>
2018-09-21 12:14:51 +0200

    #49 Remove azure storage for static files (for now)

--- arches_3d/arches_3d/settings.py
+++ arches_3d/arches_3d/settings.py
@@ -103,7 +103,7 @@ STATIC_ROOT = '/static_root'
 STATIC_URL = get_optional_env_variable('STATIC_URL') or '/media/'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
-STATICFILES_STORAGE = 'storages.backends.azure_storage.AzureStorage'
+# STATICFILES_STORAGE = 'storages.backends.azure_storage.AzureStorage'
 
 AZURE_ACCOUNT_NAME = get_env_variable('AZURE_ACCOUNT_NAME')
 AZURE_ACCOUNT_KEY = get_env_variable('AZURE_ACCOUNT_KEY')
