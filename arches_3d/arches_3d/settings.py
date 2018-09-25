e7695c10dfe9a348c6e9eaded767850c507dbd58
Vincent Meijer <meijer.vincent@gmail.com>
2018-09-25 21:11:17 +0200

    Make remote debug setting optional

--- arches_3d/arches_3d/settings.py
+++ arches_3d/arches_3d/settings.py
@@ -31,7 +31,10 @@ def get_optional_env_variable(var_name):
 
 MODE = get_env_variable('DJANGO_MODE')  # options are either "PROD" or "DEV"
 DEBUG = ast.literal_eval(get_env_variable('DJANGO_DEBUG'))
-REMOTE_DEBUG = ast.literal_eval(get_env_variable('DJANGO_REMOTE_DEBUG'))
+if get_optional_env_variable('DJANGO_REMOTE_DEBUG'):
+    REMOTE_DEBUG = ast.literal_eval(get_optional_env_variable('DJANGO_REMOTE_DEBUG'))
+else:
+    REMOTE_DEBUG = False
 
 APP_ROOT = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 STATICFILES_DIRS = (os.path.join(APP_ROOT, 'media'),) + STATICFILES_DIRS
