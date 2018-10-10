d55ff7142551c31655cdbf957fbd5dfa56cca07b
Vincent Meijer <meijer.vincent@gmail.com>
2018-10-10 13:44:08 +0200

    #67 Look for Potree model in zip file path excluding extension (instead of .zip_extracted)

--- arches_3d/arches_3d/media/js/reports/potree/potree.js
+++ arches_3d/arches_3d/media/js/reports/potree/potree.js
@@ -40,9 +40,9 @@ define([
                     self.potreeZipFiles(potreeZipFiles);
                     let modelName = potreeZipFiles[0].name;
                     let filepath = potreeZipFiles[0].src;
-                    let extractedFilePath = filepath + "_extracted"
+                    let filepathWithoutExtension = filepath.replace(/\.[^/.]+$/, "");
                     window.viewer = new Potree.Viewer(document.getElementById("potree_render_area"));
-                    potreeSetup.setupPotree(extractedFilePath + "/cloud.js", modelName)
+                    potreeSetup.setupPotree(filepathWithoutExtension + "/cloud.js", modelName)
                 }
             }
 
