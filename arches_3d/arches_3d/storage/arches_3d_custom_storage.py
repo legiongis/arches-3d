3272339c2b0ca8b3b4277034f18734658ab76ba1
Vincent Meijer <meijer.vincent@gmail.com>
2018-10-15 13:52:51 +0200

    #82 Add multithreading for upload zip method

--- arches_3d/arches_3d/storage/arches_3d_custom_storage.py
+++ arches_3d/arches_3d/storage/arches_3d_custom_storage.py
@@ -1,5 +1,7 @@
 import os
 
+from pathos.pools import ProcessPool
+
 from django.core.files.uploadedfile import SimpleUploadedFile
 from storages.backends.azure_storage import AzureStorage
 
@@ -11,27 +13,17 @@ class Arches3dCustomStorage(AzureStorage):
 
     def _save(self, name, content):
         actual_file_name = super(Arches3dCustomStorage, self)._save(name, content)
-        filepath, file_extension = os.path.splitext(actual_file_name)
+        original_filepath, file_extension = os.path.splitext(actual_file_name)
         
         if file_extension == '.zip':
             try:
                 print "Unzipping and saving contents of: " + actual_file_name
                 input_zip = ZipFile(content)
-                for file in input_zip.filelist:
-                    if not self.IsDirectory(file):
-                        output_filepath = os.path.join(filepath, file.filename)
+                pool = ProcessPool(4)
 
-                        content_type = mimetypes.guess_type(file.filename)[0]
-                        content = input_zip.open(file).read()
-
-                        memory_file = SimpleUploadedFile(
-                            name=output_filepath,
-                            content=content,
-                            content_type=content_type
-                        )
-
-                        super(Arches3dCustomStorage, self)._save(output_filepath, memory_file)
-                print "Finished uploading: " + actual_file_name
+                pool.map(self.save_file, [(zipinfo_file.filename, input_zip.open(zipinfo_file).read(), original_filepath) for zipinfo_file in input_zip.filelist])
+                    
+                print "Finished saving contents of: " + actual_file_name
 
             except BadZipfile:
                 print "Uploaded file was corrupt"
@@ -41,5 +33,20 @@ class Arches3dCustomStorage(AzureStorage):
 
         return actual_file_name
 
-    def IsDirectory(self, file):
-        return file.filename.endswith('/')
+    def save_file(self, arguments):
+        (filename, content, original_filepath) = arguments
+        if not self.IsDirectory(filename):
+            output_filepath = os.path.join(original_filepath, filename)
+
+            content_type = mimetypes.guess_type(filename)[0]
+
+            memory_file = SimpleUploadedFile(
+                name=output_filepath,
+                content=content,
+                content_type=content_type
+            )
+
+            super(Arches3dCustomStorage, self)._save(output_filepath, memory_file)
+
+    def IsDirectory(self, filename):
+        return filename.endswith('/')
