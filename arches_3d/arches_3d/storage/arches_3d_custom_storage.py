9b19ca03ddf966df4232126b314b7133476042ce
Vincent Meijer <meijer.vincent@gmail.com>
2018-10-15 13:54:23 +0200

    #82 Move code to separate method

--- arches_3d/arches_3d/storage/arches_3d_custom_storage.py
+++ arches_3d/arches_3d/storage/arches_3d_custom_storage.py
@@ -16,25 +16,29 @@ class Arches3dCustomStorage(AzureStorage):
         original_filepath, file_extension = os.path.splitext(actual_file_name)
         
         if file_extension == '.zip':
-            try:
-                print "Unzipping and saving contents of: " + actual_file_name
-                input_zip = ZipFile(content)
-                pool = ProcessPool(4)
-
-                pool.map(self.save_file, [(zipinfo_file.filename, input_zip.open(zipinfo_file).read(), original_filepath) for zipinfo_file in input_zip.filelist])
-                    
-                print "Finished saving contents of: " + actual_file_name
-
-            except BadZipfile:
-                print "Uploaded file was corrupt"
-            except Exception as e:
-                print "Upload of zip file failed: "
-                print e
+            self.extract_and_save_zip(actual_file_name, content, original_filepath)
 
         return actual_file_name
 
+    def extract_and_save_zip(self, actual_file_name, content, original_filepath):
+        try:
+            print "Unzipping and saving contents of: " + actual_file_name
+            input_zip = ZipFile(content)
+            
+            pool = ProcessPool(4)
+            pool.map(self.save_file, [(zipinfo_file.filename, input_zip.open(zipinfo_file).read(), original_filepath) for zipinfo_file in input_zip.filelist])
+
+            print "Finished saving contents of: " + actual_file_name
+
+        except BadZipfile:
+            print "Uploaded file was corrupt"
+        except Exception as e:
+            print "Upload of zip file failed: "
+            print e
+
     def save_file(self, arguments):
         (filename, content, original_filepath) = arguments
+        
         if not self.IsDirectory(filename):
             output_filepath = os.path.join(original_filepath, filename)
 
