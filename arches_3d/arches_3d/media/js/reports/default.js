0192d9e7b22fc6539c35f392b5de24c5fc84af35
Vincent Meijer <meijer.vincent@gmail.com>
2018-11-14 13:43:21 +0100

    #117 Add button to toggle report metadata section

--- arches_3d/arches_3d/media/js/reports/default.js
+++ arches_3d/arches_3d/media/js/reports/default.js
@@ -1,4 +1,11 @@
 define(['knockout', 'viewmodels/report'], function (ko, ReportViewModel) {
+
+    $(document).on("click", "#report-body-toggle-button", function() {
+        $("#report-body").toggle();
+
+
+    })
+
     return ko.components.register('default-report', {
         viewModel: function(params) {
             params.configKeys = [];
