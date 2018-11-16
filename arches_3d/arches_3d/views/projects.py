4c30045971208eb9498bf0d5e647aa7fb5b1a715
Vincent Meijer <meijer.vincent@gmail.com>
2018-11-16 14:57:59 +0100

    #109 Add project description to view. Cleanup

--- arches_3d/arches_3d/views/projects.py
+++ arches_3d/arches_3d/views/projects.py
@@ -16,41 +16,13 @@ class ProjectsView(BaseManagerView):
 
         for project in projects:
 
-            graph = Graph.objects.get(graphid='243f8689-b8f6-11e6-84a5-026d961c88e6')
-            cards = Card.objects.filter(graph=graph).order_by('sortorder')
             tiles = Tile.objects.filter(resourceinstance=project).order_by('sortorder')
-            project.permitted_cards = []
-            project.permitted_tiles = []
-
-
-            for card in cards:
-                if request.user.has_perm(perm, card.nodegroup):
-                    card.filter_by_perm(request.user, perm)
-                    project.permitted_cards.append(card)
-                    print 'Appended card: ' + card.name
 
             for tile in tiles:
                 print 'nodegroup id: ' + str(tile.nodegroup_id)
                 if str(tile.nodegroup_id) == 'fb0c163e-d138-11e8-814d-0242ac1a0004':
                     project.thumbnail_url = tile.data['fb0c1e72-d138-11e8-814d-0242ac1a0004'][0]['url']
-
-                if request.user.has_perm(perm, tile.nodegroup):
-                    tile.filter_by_perm(request.user, perm)
-                    project.permitted_tiles.append(tile)
-                    print 'Appended tile: ' 
-                    print tile.tileid
-                    print tile.nodegroup_id
-                    print tile.data
-
-        # context = self.get_context_data(
-        #     main_script='views/projects',
-        #     projects=JSONSerializer().serialize(projects, sort_keys=False),
-        # )
-        
-        # context = super(ProjectsView, self).get_context_data()
-        # context = {}
-        # context['projects']['project1'] = { 'title': 'Test Project 1', 'image_url' : 'https://prodarchesstorage.blob.core.windows.net/arches/uploadedfiles/london_thumbnail_9ha1f5q.jpg' }
-
-
+                elif str(tile.nodegroup_id) == 'aee32ff0-af95-11e8-b710-0242ac120005': 
+                    project.description = tile.data['aee33842-af95-11e8-b710-0242ac120005'] or ''
 
         return render(request, 'views/projects.htm', { 'projects': projects })
