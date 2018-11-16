90f4ea5a47a5ba0a6392c08da43e086f09d0ea87
Vincent Meijer <meijer.vincent@gmail.com>
2018-11-16 16:20:38 +0100

    #109 Add initial Heritage Sites overview page

---
+++ arches_3d/arches_3d/views/heritage_sites.py
@@ -0,0 +1,30 @@
+from django.views.generic import View
+from django.shortcuts import render
+from arches.app.views.base import BaseManagerView
+from arches.app.models.resource import Resource
+from arches.app.models.card import Card
+from arches.app.models.graph import Graph
+from arches.app.models.tile import Tile
+from arches.app.utils.betterJSONSerializer import JSONSerializer
+
+class HeritageSitesView(BaseManagerView):
+
+    def get(self, request):
+        sites = Resource.objects.filter(graph_id='fad0563b-b8f8-11e6-84a5-026d961c88e6')
+
+        for site in sites:
+            print 'site name: ' + site.displayname
+
+            tiles = Tile.objects.filter(resourceinstance=site).order_by('sortorder')
+
+            for tile in tiles:
+                print 'nodegroup id: ' + str(tile.nodegroup_id)
+                print 'tile id: ' + str(tile.tileid)
+                print tile.data
+                if str(tile.nodegroup_id) == 'fb0c163e-d138-11e8-814d-0242ac1a0004':
+                    site.thumbnail_url = tile.data['fb0c1e72-d138-11e8-814d-0242ac1a0004'][0]['url']
+                elif str(tile.nodegroup_id) == '065b7267-e746-11e6-84a6-026d961c88e6': 
+                    if tile.data['065b726b-e746-11e6-84a6-026d961c88e6'] == 'eb3bd719-c473-40b8-bf0a-cdb9ed89aba3':
+                        site.primary_description = tile.data['065b726a-e746-11e6-84a6-026d961c88e6'] or ''
+
+        return render(request, 'views/heritage-sites.htm', { 'sites': sites })
