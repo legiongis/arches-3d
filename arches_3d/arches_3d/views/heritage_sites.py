df677bf089aac6180b839e98b8abbc0f44ebe10a
Vincent Meijer <meijer.vincent@gmail.com>
2018-11-16 20:31:24 +0100

    #109 Add check for existence of image tile

--- arches_3d/arches_3d/views/heritage_sites.py
+++ arches_3d/arches_3d/views/heritage_sites.py
@@ -16,7 +16,8 @@ class HeritageSitesView(BaseManagerView):
             tiles = Tile.objects.filter(resourceinstance=site).order_by('sortorder')
             for tile in tiles:
                 if str(tile.nodegroup_id) == 'a13a9486-d134-11e8-a039-0242ac1a0004':
-                    site.thumbnail_url = tile.data['a13a9cc4-d134-11e8-a039-0242ac1a0004'][0]['url'] or ''
+                    if len(tile.data['a13a9cc4-d134-11e8-a039-0242ac1a0004']) > 0:
+                        site.thumbnail_url = tile.data['a13a9cc4-d134-11e8-a039-0242ac1a0004'][0]['url'] or ''
                 elif str(tile.nodegroup_id) == '065b7267-e746-11e6-84a6-026d961c88e6': 
                     if tile.data['065b726b-e746-11e6-84a6-026d961c88e6'] == 'eb3bd719-c473-40b8-bf0a-cdb9ed89aba3':
                         site.primary_description = tile.data['065b726a-e746-11e6-84a6-026d961c88e6'] or ''
