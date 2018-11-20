03d3fd828f2511daedcbbf58581fe1e91d3cf54d
Matthew Vincent <mvincent@basecamp.com>
2018-11-20 19:53:00 +0000

    primary redesign of home page and team page

--- arches_3d/arches_3d/urls.py
+++ arches_3d/arches_3d/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
+from arches_3d.views import brochure
+
 
 from arches_3d.views import projects, heritage_sites, meta_data
 
@@ -9,5 +11,6 @@ urlpatterns = [
     url(r'^projects$', projects.ProjectsView.as_view(), name="projects"),
     url(r'^sites$', heritage_sites.HeritageSitesView.as_view(), name="sites"),
     url(r'^node_values$', meta_data.get_node_values, name="node_values"),
-
+    url(r'^team/', brochure.team, name='team'),
+    url(r'^equipment/', brochure.equipment, name='equipment')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
