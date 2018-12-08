79034455cf2fbd7624b0cbc587045b99f87128be
Vincent Meijer <meijer.vincent@gmail.com>
2018-12-08 13:48:19 +0100

    #143 Move metadata section below Related Resources. Hide metadata by default. Restyle button. Scroll to metadata section on button click.

--- arches_3d/arches_3d/media/js/reports/default.js
+++ arches_3d/arches_3d/media/js/reports/default.js
@@ -1,8 +1,12 @@
 define(['knockout', 'viewmodels/report'], function (ko, ReportViewModel) {
 
     $(document).on("click", "#report-body-toggle-button", function() {
-        $("#report-body").toggle();
-    })
+        $("#report-body").toggle(function(){
+            $('.report-metadata-section').get(0).scrollIntoView({ behavior: 'smooth', block: 'start' });
+        });
+
+        $('#report-body-toggle-button-icon').toggleClass("fa-plus fa-minus");
+    });
 
     return ko.components.register('default-report', {
         viewModel: function(params) {
