bbf1cc1fcc4eb2e89b84f5f9717868ccef2b7838
Vincent Meijer <meijer.vincent@gmail.com>
2019-01-12 15:15:14 +0100

    #165 Add initial Before/After Image header template

---
+++ arches_3d/arches_3d/media/js/reports/before-after-image.js
@@ -0,0 +1,86 @@
+define([
+    'underscore',
+    'jquery',
+    'knockout',
+    'knockout-mapping',
+    'viewmodels/report',
+    'arches',
+    'twentytwenty',
+    'twentytwenty-move'
+], function(_, $, ko, koMapping, ReportViewModel, arches) {
+    return ko.components.register('before-after-image-report', {
+        viewModel: function(params) {
+            var self = this;
+            params.configKeys = ['nodes'];
+            ReportViewModel.apply(this, [params]);
+
+            self.imgs = ko.computed(function() {
+                var imgs = [];
+                var nodes = self.nodes();
+                self.tiles().forEach(function(tile) {
+                    _.each(tile.data, function(val, key) {
+                        val = koMapping.toJS(val);
+                        if (Array.isArray(val)) {
+                            val.forEach(function(item) {
+                                if (item.status &&
+                                    item.type &&
+                                    item.status === 'uploaded' &&
+                                    item.type.indexOf('image') > -1 &&
+                                    _.contains(nodes, key)
+                                ) {
+                                    var img = {
+                                        src: item.url,
+                                        alt: item.name
+                                    }
+
+                                    if (key === "780108d0-1661-11e9-bf95-0242ac140004"){
+                                        img.type = "before"
+                                    }
+                                    else if (key === "8ce12c52-1662-11e9-bf95-0242ac140004"){
+                                        img.type = "after"
+                                    }
+
+                                    imgs.push(img);
+                                }
+                            });
+                        }
+                    }, self);
+                }, self);
+                if (imgs.length === 0) {
+                    imgs = [{
+                        src: arches.urls.media + 'img/photo_missing.png',
+                        alt: ''
+                    }];
+                }
+                return imgs;
+            });
+
+            if (self.imgs().length > 1) {
+                var beforeImg = self.imgs().find(img => {
+                    return img.type === 'before'
+                });
+                var afterImage = self.imgs().find(img => {
+                    return img.type === 'after'
+                });
+
+                $('#before-image').attr('src', beforeImg.src);
+                $('#before-image').attr('alt', beforeImg.alt);
+
+                $('#after-image').attr('src', afterImage.src);
+                $('#after-image').attr('alt', afterImage.alt);
+
+                // $('#before-after-image-container').twentytwenty();
+            }
+        },
+        template: {
+            require: 'text!report-templates/before-after-image'
+        }
+    });
+});
+
+
+$(document).on('load', function(){
+    console.log('window');
+    $('#before-after-image-container').twentytwenty();
+    $(window).trigger("resize");
+});
