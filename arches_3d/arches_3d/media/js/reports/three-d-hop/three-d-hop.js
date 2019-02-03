5257448b29415c23b6d0801b37d447e8ea588f46
Vincent Meijer <meijer.vincent@gmail.com>
2019-02-03 19:02:24 +0100

    #116 Add methods for parsing config items and building the config object

--- arches_3d/arches_3d/media/js/reports/three-d-hop/three-d-hop.js
+++ arches_3d/arches_3d/media/js/reports/three-d-hop/three-d-hop.js
@@ -4,11 +4,15 @@ define([
     'viewmodels/report',
     'arches',
     'reports/three-d-hop/three-d-hop-setup',
+    'trackball_sphere',
+    'trackball_turntable',
+    'trackball_turntable_pan',
+    'trackball_pantilt',
     'knockstrap',
     'bindings/chosen'
-], function (_, ko, ReportViewModel, arches, threeDHopSetup) {
+], function (_, ko, ReportViewModel, arches, threeDHopSetup, trackball_sphere, trackball_turntable, trackball_turntable_pan, trackball_pantilt) {
 
-        function getValue(valueid){
+        function getConceptValue(valueid){
             var val;
             $.ajax({
                 type: 'GET',
@@ -20,6 +24,46 @@ define([
                 }
             });
             return val;
+        }
+
+        function addProperty(object, key, value, index) {
+            var keys = key.split('.');
+            object = initializeParentProperties(object, keys);
+
+            if (index != null){
+                addArrayProperty(object, keys, value, index)
+            }
+            else {
+                object[keys[0]] = value;
+            }
+        }
+
+        function initializeParentProperties(object, keys){
+            while (keys.length > 1){
+                var k = keys.shift();
+                if (!object.hasOwnProperty(k)) {
+                    object[k] = {};
+                }
+
+                object = object[k];
+            }
+            return object;
+        }
+
+        function addArrayProperty(object, keys, value, index){
+            var propertyName = keys[0];
+            initializeArrayIfNotExists(object, propertyName);
+            object[propertyName][index] = value;
+        }
+
+        function initializeArrayIfNotExists(object, propertyName){
+            if (!object.hasOwnProperty(propertyName)){
+                object[propertyName] = [];
+            }
+        }
+
+        function removeDotsFromString(string) {
+            return string.replace('.', '-');
         }
 
         return ko.components.register('three-d-hop-report', {
@@ -36,6 +80,10 @@ define([
                 self.report.get('tiles').forEach(function (tile) {
                     _.each(tile.data, function (val, key) {
 
+                        if (val == null){
+                            return;
+                        }
+
                         // 3D model file
                         if (Array.isArray(val)) {
                             val.forEach(function (item) {
@@ -49,13 +97,13 @@ define([
                                         alt: item.name
                                     });
 
-                                    var mesh = new Object();
-                                    mesh[item.name] = { url: item.url};
-                                    config.meshes.push(mesh);
+                                    var mesh = { url: item.url };
+                                    var meshName = removeDotsFromString(item.name);
+                                    addProperty(config, `meshes.${meshName}`, mesh)
 
-                                    var instance = new Object;
-                                    instance[item.name] = { mesh: item.name}
-                                    config.modelInstances.push(instance);
+                                    var instance = { mesh: meshName };
+                                    var instanceName = removeDotsFromString(item.name);
+                                    addProperty(config, `modelInstances.${instanceName}`, instance)
                                 }
                             });
                             return;
@@ -66,115 +114,117 @@ define([
 
                         // TrackballType
                         if (key === '1123258a-226e-11e9-8639-0242ac170002'){
-                            config.trackball.type = getValue(val);
+                            var conceptValue = getConceptValue(val);
+                            var conceptAsType = window[conceptValue];
+                            addProperty(config, 'trackball.type', conceptAsType);
                             return;
                         }
 
                         // Start Phi
                         if (key === '4fef73c2-226e-11e9-9e1e-0242ac170002'){
-                            config.trackball.trackOptions.startPhi = val;
+                            addProperty(config, 'trackball.trackOptions.startPhi', val);
                             return;
                         }    
 
                         // Start Theta
                         if (key === '77472ef6-226e-11e9-8bd2-0242ac170002'){
-                            config.trackball.trackOptions.startTheta = val;
+                            addProperty(config, 'trackball.trackOptions.startTheta', val);
                             return;
                         }
 
                         // Start Distance
                         if (key === '84a2c6fa-226e-11e9-b4cd-0242ac170002'){
-                            config.trackball.trackOptions.startDistance = val;
+                            addProperty(config, 'trackball.trackOptions.startDistance', val);
                             return;
                         }
 
                         // Start Pan X
                         if (key === '92d84f1a-226e-11e9-8639-0242ac170002'){
-                            config.trackball.trackOptions.startPanX = val;
+                            addProperty(config, 'trackball.trackOptions.startPanX', val);
                             return;
                         }
 
                         // Start Pan Y
                         if (key === 'a4030654-226e-11e9-b29f-0242ac170002'){
-                            config.trackball.trackOptions.startPanY = val;
+                            addProperty(config, 'trackball.trackOptions.startPanY', val);
                             return;
                         }
 
                         // Start Pan Z
                         if (key === 'b772a67c-226e-11e9-ab9d-0242ac170002'){
-                            config.trackball.trackOptions.startPanZ = val;
+                            addProperty(config, 'trackball.trackOptions.startPanZ', val);
                             return;
                         }
 
                         // Min Dist
                         if (key === '43195b86-26e7-11e9-b29f-0242ac170002'){
-                            config.trackball.trackOptions.minMaxDist[0] = val;
+                            addProperty(config, 'trackball.trackOptions.minMaxDist', val, 0);
                             return;
                         }
 
                         // Max Dist
                         if (key === '67f05c8e-26e7-11e9-8639-0242ac170002'){
-                            config.trackball.trackOptions.minMaxDist[1] = val;
+                            addProperty(config, 'trackball.trackOptions.minMaxDist', val, 1);
                             return;
                         }
 
                         // Min Phi
                         if (key === '97b84d6e-26e7-11e9-ab9d-0242ac170002'){
-                            config.trackball.trackOptions.minMaxPhi[0] = val;
+                            addProperty(config, 'trackball.trackOptions.minMaxPhi', val, 0);
                             return;
                         }
 
                         // Max Phi
                         if (key === 'b18eb494-26e7-11e9-b4cd-0242ac170002'){
-                            config.trackball.trackOptions.minMaxPhi[1] = val;
+                            addProperty(config, 'trackball.trackOptions.minMaxPhi', val, 1);
                             return;
                         }
 
                         // Min Theta
                         if (key === '1c873974-26e8-11e9-8bd2-0242ac170002'){
-                            config.trackball.trackOptions.minMaxTheta[0] = val;
+                            addProperty(config, 'trackball.trackOptions.minMaxTheta', val, 0);
                             return;
                         }
 
                         // Max Theta
                         if (key === '2d7ebcb6-26e8-11e9-8639-0242ac170002'){
-                            config.trackball.trackOptions.minMaxTheta[1] = val;
+                            addProperty(config, 'trackball.trackOptions.minMaxTheta', val, 1);
                             return;
                         }
 
                         // Min Pan X
                         if (key === '49977ba4-26e8-11e9-ad1e-0242ac170002'){
-                            config.trackball.trackOptions.minMaxPanX[0] = val;
+                            addProperty(config, 'trackball.trackOptions.minMaxPanX', val, 0);
                             return;
                         }
 
                         // Max Pan X
                         if (key === '5ca61af2-26e8-11e9-8639-0242ac170002'){
-                            config.trackball.trackOptions.minMaxPanX[1] = val;
+                            addProperty(config, 'trackball.trackOptions.minMaxPanX', val, 1);
                             return;
                         }
 
                         // Min Pan Y
                         if (key === '743bc66c-26ed-11e9-8639-0242ac170002'){
-                            config.trackball.trackOptions.minMaxPanY[0] = val;
+                            addProperty(config, 'trackball.trackOptions.minMaxPanY', val, 0);
                             return;
                         }
 
                         // Max Pan Y
                         if (key === '8b10fdf8-26ed-11e9-8c52-0242ac170002'){
-                            config.trackball.trackOptions.minMaxPanY[1] = val;
+                            addProperty(config, 'trackball.trackOptions.minMaxPanY', val, 1);
                             return;
                         }
 
                         // Min Pan Z
                         if (key === 'b1d2d47a-26ed-11e9-8c52-0242ac170002'){
-                            config.trackball.trackOptions.minMaxPanZ[0] = val;
+                            addProperty(config, 'trackball.trackOptions.minMaxPanZ', val, 0);
                             return;
                         }
                         
                         // Max Pan Z
                         if (key === 'c00d79f0-26ed-11e9-b29f-0242ac170002'){
-                            config.trackball.trackOptions.minMaxPanZ[1] = val;
+                            addProperty(config, 'trackball.trackOptions.minMaxPanZ', val, 1);
                             return;
                         }
 
@@ -183,75 +233,81 @@ define([
 
                         // Center Mode
                         if (key === 'af717052-2273-11e9-b4cd-0242ac170002'){
-                            config.space.centerMode = getValue(val);
+                            addProperty(config, 'trackball.space.centerMode', getConceptValue(val));
                             return;
                         }
 
                         // Explicit Center X
                         if (key === '4badaf1c-2274-11e9-ad1e-0242ac170002'){
-                            config.space.explicitCenter[0] = val;
+                            addProperty(config, 'trackball.space.explicitCenter', val, 0);
                             return;
                         }
 
                         // Explicit Center Y
                         if (key === '41e3ad9c-2274-11e9-ad1e-0242ac170002'){
-                            config.space.explicitCenter[1] = val;
+                            addProperty(config, 'trackball.space.explicitCenter', val, 1);
                             return;
                         }
 
                         // Explicit Center Z
                         if (key === 'cc736a92-2274-11e9-b29f-0242ac170002'){
-                            config.space.explicitCenter[2] = val;
+                            addProperty(config, 'trackball.space.explicitCenter', val, 2);
                             return;
                         }
 
 
                         // Radius Mode
                         if (key === 'd3843830-2273-11e9-b7c2-0242ac170002'){
-                            config.space.radiusMode = getValue(val);
+                            addProperty(config, 'trackball.space.radiusMode', getConceptValue(val));
                             return;
                         }
 
                         // Explicit Radius X
                         if (key === '1553379c-2275-11e9-9e1e-0242ac170002'){
-                            config.space.explicitRadius[0] = val;
+                            addProperty(config, 'trackball.space.explicitRadius', val, 0);
                             return;
                         }
 
                         // Explicit Radius Y
                         if (key === '8954efdc-2275-11e9-b7c2-0242ac170002'){
-                            config.space.explicitRadius[1] = val;
+                            addProperty(config, 'trackball.space.explicitRadius', val, 1);
                             return;
                         }
 
                         // Explicit Radius Z
                         if (key === '97d7004a-2275-11e9-8bd2-0242ac170002'){
-                            config.space.explicitRadius[2] = val;
+                            addProperty(config, 'trackball.space.explicitRadius', val, 2);
                             return;
                         }
 
 
                         // Camera Type
                         if (key === '0b40d042-2276-11e9-8bd2-0242ac170002'){
-                            config.space.cameraType = getValue(val);
+                            addProperty(config, 'trackball.space.cameraType', getConceptValue(val));
                             return;
                         }
 
                         // Camera FOV
                         if (key === '758debf6-2276-11e9-9e1e-0242ac170002'){
-                            config.space.cameraFOV = val;
+                            addProperty(config, 'trackball.space.cameraFOV', val);
                             return;
                         }
 
                         // Camera Near
                         if (key === 'cb30e982-2276-11e9-ad1e-0242ac170002'){
-                            config.space.cameraNearFar[0] = val;
+                            addProperty(config, 'trackball.space.cameraNearFar', val, 0);
                             return;
                         }
                         
                         // Camera Far
                         if (key === 'da582a92-2276-11e9-b4cd-0242ac170002'){
-                            config.space.cameraNearFar[1] = val;
+                            addProperty(config, 'trackball.space.cameraNearFar', val, 1);
+                            return;
+                        }
+
+                        // Scene Lighting
+                        if (key === 'd316cfe2-27c9-11e9-8639-0242ac170002'){
+                            addProperty(config, 'space.sceneLighting', val);
                             return;
                         }
 
