babb9c5516be7b7b0078a65bea31ecba9057161f
adam cox <mr.adamcox@gmail.com>
2019-04-27 09:31:35 -0500

    switch default basemap on db reinstall

--- arches_3d/arches_3d/management/commands/load.py
+++ arches_3d/arches_3d/management/commands/load.py
@@ -2,6 +2,7 @@ import os
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from django.conf import settings
+from arches.app.models.models import MapLayer
 
 import logging
 
@@ -21,6 +22,7 @@ class Command(BaseCommand):
             call_command('packages',operation="setup_db")
             self.load_reference_data()
             call_command('packages',operation="import_graphs")
+            self.switch_basemap_layer()
 
     def load_reference_data(self):
         thesauri = os.path.join(settings.APP_ROOT,"db","schemes","thesauri")
@@ -34,3 +36,13 @@ class Command(BaseCommand):
             if f.endswith(".rdf"):
                 call_command("packages",operation="import_reference_data",
                     source=os.path.join(collections,f))
+    
+    def switch_basemap_layer(slef):
+        
+        streets = MapLayer.objects.get(name="streets")
+        streets.addtomap = False
+        streets.save()
+        
+        satellite = MapLayer.objects.get(name="satellite")
+        satellite.addtomap = True
+        satellite.save()
