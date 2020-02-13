4cca6741b3262a4743bf6c3274e3b4ccc28926ef
Angela Labrador <angela.labrador@coherit.com>
2020-02-13 12:27:16 -0500

    added help annotation to potree viewer and fixed link on homepage

--- arches_3d/arches_3d/media/js/reports/potree/potree-setup.js
+++ arches_3d/arches_3d/media/js/reports/potree/potree-setup.js
@@ -75,7 +75,7 @@ define([
 
             viewer.setEDLEnabled(true);
             viewer.setBackground('gradient'); // ["skybox", "gradient", "black", "white"];
-            viewer.setDescription('');
+            viewer.setDescription('Left click to Pan. Right Click to Orbit. Click on Menu icon in top left for more in-depth tools, such as measuring. Click on icon in bottom left to toggle full screen.');
 			viewer.setNavigationMode(Potree.EarthControls);
             viewer.loadSettingsFromURL();
 
